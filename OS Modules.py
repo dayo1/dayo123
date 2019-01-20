07f7a65f744a40e1763c159d6f5beb15004ea1d5
Dayo Udiani <dayo.udiani@gmail.com>
2019-01-19 22:08:58 -0500

    Add files via upload

---
+++ OS Modules.py
@@ -0,0 +1,40 @@
+"""
+import os
+print(os.name)
+
+"""
+
+"""
+import os
+os.getcwd()
+
+"""
+
+"""
+import os
+os.mkdir("pract1")
+"""
+
+"""
+import os
+os.chdir("pract1")
+print(os.getcwd())
+
+print(os.listdir())
+
+
+for dirpath, dirnames, filenames in os.walk("C:/Users/User/PycharmProjects"):
+    print ('Current path:', dirpath)
+    print('Directories:', dirnames)
+    print('Files:', filenames)
+    print()
+
+"""
+
+import os
+
+from datetime import datetime
+print(os.stat("text3.txt"))
+time = os.stat("text3.txt").st_mtime
+print(datetime.fromtimestamp(time))
+
