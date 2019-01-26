f0855b17c5f716a1efc3a44ca23f60ae1eca375f
User <dayo.udiani@gmail.com>
2019-01-25 23:30:40 -0500

    First Commit

---
+++ Assignment_Wk_14.py
@@ -0,0 +1,25 @@
+
+"""
+# os.listdir
+import os
+import csv
+for file in os.listdir("C:/Users/User/PycharmProjects/dayo123-1"):
+    if file.endswith(".csv"):
+        print (file)
+"""
+
+
+
+
+"""
+import os
+os.listdir()
+
+print os.listdir()
+
+"""
+
+
+
+
+
--- OS Modules.py
+++ OS Modules.py
@@ -39,7 +39,7 @@ time = os.stat("text3.txt").st_mtime
 print(datetime.fromtimestamp(time))
 
 import os
-os.mkdir("pract2")
+os.mkdir("pract3")
 
-os.chdir("pract2")
+os.chdir("pract3")
 print(os.getcwd())
